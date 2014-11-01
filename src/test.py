587a1db27227aeee09dfe6591c582dfd063ae8c9
N1amr <n1amr1@gmail.com>
2014-11-01 18:11:47 +0200

    pythonProject

--- src/test.py
+++ src/test.py
@@ -1,14 +1,3 @@
-'''
-Created on Nov 1, 2014
+import math
 
-@author: N1amr
-'''
-
-class MyClass:
-    def __init__(self):
-        self.x = 0
-
-y = MyClass()
-z = MyClass()
-print y.x
-print z.x
+print math.log(64,2);
